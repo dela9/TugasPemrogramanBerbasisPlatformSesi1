892534a419f3fd2c163e16643b7d3ed61ecf7f48
dela9 <124057172+dela9@users.noreply.github.com>
2023-09-29 21:08:46 +0700

    Add files via upload

---
+++ tugas.1,nodeJs.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama ='salma dela septiani'
+let nim = '20220040090'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+//aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 39;
+let b = 9;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +(a+b))
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' + (a-b))
